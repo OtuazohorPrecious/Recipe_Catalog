a2ebbd4c7102117cba7b7b269d661d3de34981e1
Otuazohor Precious <otuapreshy@gmail.com>
2025-05-20 18:33:55 +0300

    Add production dependdencies and update recipes model

--- meal_plans/admin.py
+++ meal_plans/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Meal, MealPlan
 # Register your models here.
+
+
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'meal_type')
+
+@admin.register(MealPlan)
+class MealPlanAdmin(admin.ModelAdmin):
+    list_display = ('start_date', 'end_date', 'created_at')
